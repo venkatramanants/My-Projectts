99d3060ff38e08df2f9b63109cdc75485c49ba7a
venkatramanants <99989292+venkatramanants@users.noreply.github.com>
2022-02-28 21:19:08 +0530

    Add files via upload

---
+++ innerClassAssisted3.java
@@ -0,0 +1,19 @@
+package innerclass3;
+
+abstract class AnonymousInnerClass {
+	   public abstract void display();
+	}
+
+
+	public class innerClassAssisted3 {
+
+	public static void main(String[] args) {
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	            System.out.println("Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
