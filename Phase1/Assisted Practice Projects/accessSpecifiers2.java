6a0b65a63170ee1ba656cde4c9864d9608335a04
venkatramanants <99989292+venkatramanants@users.noreply.github.com>
2022-02-28 20:33:34 +0530

    Add files via upload

---
+++ Phase1/Assisted Practice Projects/accessSpecifiers2.java
@@ -0,0 +1,21 @@
+package Access2;
+class priaccessspecifier 
+{ 
+   private void display() 
+    { 
+        System.out.println("You are using private access specifier"); 
+    } 
+} 
+
+public class accessSpecifiers2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Private Access Specifier");
+		priaccessspecifier  obj = new priaccessspecifier(); 
+        
+
+	}
+}
+
+
