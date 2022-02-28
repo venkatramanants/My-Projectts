55dce736b18cd14030c58019be123ebcc19d3825
venkatramanants <99989292+venkatramanants@users.noreply.github.com>
2022-02-28 20:48:55 +0530

    Add files via upload

---
+++ Phase1/Assisted Practice Projects/accessSpecifiers3.java
@@ -0,0 +1,13 @@
+package Pack2;
+
+import Pack1.*;
+
+public class accessSpecifiers3 extends proaccessspecifiers {
+
+	public static void main(String[] args) {
+		accessSpecifiers3 obj = new accessSpecifiers3 ();   
+	       obj.display();  
+	}
+
+}
+
