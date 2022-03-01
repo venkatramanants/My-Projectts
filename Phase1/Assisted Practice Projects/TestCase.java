9943006e9f663f2f3cb4c0848732f9ff07eb980a
venkatramanants <99989292+venkatramanants@users.noreply.github.com>
2022-03-01 23:29:27 +0530

    Add files via upload

---
+++ Phase1/Assisted Practice Projects/TestCase.java
@@ -0,0 +1,28 @@
+package Diamond;
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class TestCase implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        TestCase ob = new TestCase(); 
+        ob.show(); 
+    } 
+}
