08afd7fcbf7549269712f52b5715624092ee1e32
venkatramanants <99989292+venkatramanants@users.noreply.github.com>
2022-03-02 17:51:40 +0530

    Add files via upload

--- Phase1/Assisted Practice Projects/Array.java
+++ Phase1/Assisted Practice Projects/Array.java
@@ -1,21 +1,28 @@
 package Array;
-
-public class Array {
-
-public static void main(String[] args) {
-
-//single-dimensional array
-int a[]= {1,2,3,4,5};
-for(int i=0;i<5;i++) {
-System.out.println("Elements of array a: "+a[i]);
+class RotateArray { 
+public void rotate(int[] nums, int k) {
+    		if(k > nums.length) 
+       			k=k%nums.length;
+ 		int[] result = new int[nums.length];
+ 		for(int i=0; i < k; i++){
+        			result[i] = nums[nums.length-k+i];
+ 		}
+ 		int j=0;
+    		for(int i=k; i<nums.length; i++){
+        			result[i] = nums[j];
+j++;
+    		}
+ 		System.arraycopy( result, 0, nums, 0, nums.length );
 }
-
-
-//multidimensional array
-int[][] b = {
-            {2, 4, 6}, 
-            {3, 6, 9} };
-      
-      System.out.println("\nLength of row 2: " + b[0].length);
-      }
+} 
+public class Array
+{
+	public static void main(String[] args) {
+		RotateArray r = new RotateArray();
+        		int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        		r.rotate(arr, 5); 
+        		for(int i=0;i<arr.length;i++){
+            			System.out.print(arr[i]+" ");
+        		}
+	}
 }
